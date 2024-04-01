e18b2268ea796a2033524d5f666f7fa4f7cba849
stxrkwas <maria.silva4476@etec.sp.gov.br>
2024-04-01 19:25:25 -0300

    Ainda não sei o que estoy haciendo

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('index', function () {
-    return view('index');
+Route::get('home', function () {
+    return view('home');
 });
