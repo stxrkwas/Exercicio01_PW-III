ab8f2847502dc16ad8ce032613012ef514960387
Malu⚡ <124840632+stxrkwas@users.noreply.github.com>
2024-04-08 11:05:29 -0300

    Update home.blade.php

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -7,6 +7,8 @@
     <title>Home - Etec da Zona Leste</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/css/home.css">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
 </head>
 
 <body>
@@ -25,8 +27,8 @@
             </div>
         </div>
     </div>
-    <nav class="navbar navbar-expand-lg bg-body-white">
-        <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg bg-body-white ">
+        <div class="container-fluid ">
 
             <!-- Navbar com a logo da ETEC -->
             <a class="navbar-brand" href="#">
@@ -36,17 +38,19 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <div class="collapse navbar-collapse align-items-right" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
                     <!-- Início -->
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index">Início</a>
+                        <a class="nav-link active" style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" aria-current="page" href="home">Início</a>
                     </li>
+
+                    <!-- <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a> -->
 
                     <!-- Cursos -->
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="cursos"  style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Cursos
                         </a>
 
@@ -64,7 +68,7 @@
 
                     <!-- Instituição -->
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" data-bs-toggle="dropdown" aria-expanded="false">
                             Instituição
                         </a>
 
@@ -80,8 +84,8 @@
                     </li>
 
                     <!-- Departamentos -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <li class="nav-item dropdown ">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" data-bs-toggle="dropdown" aria-expanded="false">
                             Departamentos
                         </a>
 
@@ -95,26 +99,21 @@
 
                     <!-- Oportunidades -->
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index">Oportunidades</a>
+                        <a class="nav-link active" style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" aria-current="page" href="index">Oportunidades</a>
                     </li>
 
                     <!-- Vestibulinho -->
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index">Vestibulinho</a>
+                        <a class="nav-link active" style="margin-top: 5px; margin-left: 10px; margin-right: 100px;" aria-current="page" href="index">Vestibulinho</a>
                     </li>
             </div>
         </div>
     </nav>
 
     <main>
-  <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
-    <div class="col-md-6 p-lg-5 mx-auto my-5">
-        <img src="/img/home.etec.webp" ></img>
-      </div>
-    </div>
-    <div class="product-device shadow-sm d-none d-md-block"></div>
-    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
-  </div>
+        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center" style= "width: 100%;">
+            <img src="/img/home.etec.webp" class="img-fluid" width="100%;" height="100%;"></img>
+        </div>
 </body>
 
 </html>
