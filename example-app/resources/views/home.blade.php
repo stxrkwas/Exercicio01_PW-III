03c0a3bb3224c2e1fdfc1ee0b225fe725facb23a
stxrkwas <maria.silva4476@etec.sp.gov.br>
2024-04-08 20:29:31 -0300

    Já pode ir jantar?

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -7,7 +7,6 @@
     <title>Home - ETEC Zona Leste</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="/css/home.css">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
 </head>
 
@@ -43,13 +42,13 @@
 
                         <!-- Cursos da ETEC -->
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Administração</a></li>
-                            <li><a class="dropdown-item" href="#">Contabilidede</a></li>
-                            <li><a class="dropdown-item" href="#">Novotec Desenvolvimento de Sistemas AMS</a></li>
-                            <li><a class="dropdown-item" href="#">Desenvolvimento de Sistemas</a></li>
-                            <li><a class="dropdown-item" href="#">Logística</a></li>
-                            <li><a class="dropdown-item" href="#">Recursos Humanos</a></li>
-                            <li><a class="dropdown-item" href="#">Serviços Jurídicos</a></li>
+                            <li><a class="dropdown-item" href="home">Administração</a></li>
+                            <li><a class="dropdown-item" href="home">Contabilidede</a></li>
+                            <li><a class="dropdown-item" href="home">Novotec Desenvolvimento de Sistemas AMS</a></li>
+                            <li><a class="dropdown-item" href="home">Desenvolvimento de Sistemas</a></li>
+                            <li><a class="dropdown-item" href="home">Logística</a></li>
+                            <li><a class="dropdown-item" href="home">Recursos Humanos</a></li>
+                            <li><a class="dropdown-item" href="home">Serviços Jurídicos</a></li>
                         </ul>
                     </li>
 
@@ -98,20 +97,16 @@
     </nav>
 
     <!-- Imagem: -->
-    <img src="/img/home.etec.webp" class="img-fluid" width="100%;" height="100%;"></img>
-    
-    <!-- <div class="position-relative overflow-hidden text-center"> p-3 p-md-5 m-md-3 -->
-        <!-- <img src="/img/home.etec.webp" class="img-fluid" width="100%;" height="100%;"></img>
-    </div> -->
+    <img class="img-fluid" src="/img/home.etec.webp" width="100%;" height="auto"></img>
 
     <!-- Barra de pesquisa:  -->
-    <div class="container bg-body-tertiary">
-        <div class="row bg-body-tertiary"> <!-- align-items-center -->
+    <div class="container-fluid bg-body-tertiary">
+        <div class="row bg-body-tertiary">
             <div class="col">&nbsp;</div>
             <div class="col bg-body-tertiary">
                 <div class="container-fluid mt-5 mb-5 bg-body-tertiary">
                     <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="O que está buscando?" aria-label="Search" height="65px;">
+                        <input class="form-control me-2" type="search" placeholder="O que está buscando?" aria-label="Search" style="height: 60px; width: 500px;">
                     </form>
                 </div>
             </div>
@@ -119,19 +114,78 @@
         </div>
     </div>
 
-    <!-- Texto e imagem: -->
+    <!-- (1) Texto e imagem: -->
     <div class="row featurette mt-5">
-        <div class="col-md-7 order-md-2 mt-5">
-            <h3 class="featurette-heading fw-bold lh-1">O que a ETEC pode fazer pelo seu FUTURO ainda HOJE?</h3>
-            <p class="lead lh-base fw-normal text-justify mt-4">Sabemos que às vezes pensar no futuro pode parecer assustador. Traçamos carreiras e planos 
-                profissionais a todo momento em nossos pensamentos, numa busca constante pelo caminho que nos trará o melhor. Mas saiba que a ETEC Zona Leste conhece 
-                os seus anseios e, está aqui, disposta a te apresentar um novo caminho! O futuro não precisa ser assustador. Suas habilidades técnicas e profissionais 
-                podem ser desenvolvidas enquanto você aprende com a gente. Em nossa instituição, nós valorizamos os pequenos passos. 
+        <div class="col-md-7 order-md-2 mt-3 ms-5">
+            <h3 class="featurette-heading fw-bold lh-base ms-5" style="text-align: justify;">O que a ETEC pode fazer pelo seu FUTURO ainda HOJE?</h3>
+            <p class="lead lh-base fw-normal text-justify mt-4 ms-5">Sabemos que às vezes pensar no futuro pode parecer assustador.
+                Traçamos carreiras e planos profissionais a todo momento em nossos pensamentos,
+                numa busca constante pelo caminho que nos trará o melhor. Mas saiba que a ETEC Zona
+                Leste conhece os seus anseios e, está aqui, disposta a te apresentar um novo caminho!
+                O futuro não precisa ser assustador. Suas habilidades técnicas e profissionais podem
+                ser desenvolvidas enquanto você aprende com a gente. Em nossa instituição, nós valorizamos os pequenos passos.
                 Dê o seu primeiro passo hoje em direção ao futuro. Faça parte da ETEC!</p>
         </div>
-        <div class="col-md-5 order-md-1 mt-5">
-            <img src="/img/image.png" class="rounded float-start"></img>
+        <div class="col-md-3 order-md-1 mt-3 ms-5">
+        <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto ms-5" width="600" height="600" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false" src="/img/image1.png"></img>
         </div>
+    </div>
+
+    <!-- (2) Texto e imagem: -->
+    <div class="row featurette mt-5">
+        <div class="col-md-7 order-md-1 mt-3 ms-5">
+            <h3 class="featurette-heading fw-bold lh-base ms-5" style="text-align: justify;">Excelência Educacional</h3>
+            <p class="lead lh-base fw-normal text-justify mt-4 ms-5">Na ETEC Zona Leste, a excelência educacional é a nossa prioridade máxima. 
+                Nossas aulas são ministradas por professores altamente qualificados, proporcionando uma educação satisfatória que combina 
+                conhecimentos acadêmicos sólidos com habilidades práticas essenciais para o mercado de trabalho.</p>
+        </div>
+        <div class="col-md-3 order-md-2 mt-3 ms-5">
+        <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="600" height="600" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false" src="/img/image2.png"></img>
+        </div>
+    </div>
+
+    <!-- (3) Texto e imagem: -->
+    <div class="row featurette mt-5">
+        <div class="col-md-7 order-md-2 mt-3 ms-5">
+            <h3 class="featurette-heading fw-bold lh-base ms-5">Para além da sala de aula</h3>
+            <p class="lead lh-base fw-normal text-justify mt-4 ms-5">Buscamos levar o conhecimento para além da sala de aula tradicional. 
+                Incentivamos o cultivo de conexões valiosas por meio de networking e oportunidades de carreira. Além do sucesso acadêmico, 
+                nos preocupamos com o desenvolvimento pessoal de nosso corpo estudantil, 
+                oferecendo suporte abrangente, orientação vocacional e programas extracurriculares.</p>
+        </div>
+        <div class="col-md-3 order-md-1 mt-3 ms-5">
+        <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto ms-5" width="600" height="600" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false" src="/img/image3.png"></img>
+        </div>
+    </div>
+
+    <!-- (4) Texto e imagem: -->
+    <div class="row featurette mt-5">
+        <div class="col-md-7 order-md-1 mt-3 ms-5">
+            <h3 class="featurette-heading fw-bold lh-base ms-5" style="text-align: justify;">Ambientes favoráveis</h3>
+            <p class="lead lh-base fw-normal text-justify mt-4 ms-5">Contamos com uma infraestrutura moderna, laboratórios e salas equipadas 
+                com televisões, computadores, notebooks, impressoras 3D e muitos outros componentes propícios aos projetos que desenvolvemos 
+                durante a jornada dos estudantes. Ademais, nossa instituição é envolta em um grande jardim, com árvores de pequeno e médio porte, 
+                plantas e flores, provendo ao nosso corpo estudantil um ambiente mais acolhedor e aconchegante. 
+                Na ETEC Zona Leste, você encontrará um espaço favorável para o impulsionar seu crescimento acadêmico e profissional.</p>
+        </div>
+        <div class="col-md-3 order-md-2 mt-3 ms-5">
+        <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="600" height="600" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false" src="/img/image4.png"></img>
+        </div>
+    </div>
+
+    <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <p class="col-md-4 mb-0 text-body-secondary">&copy; 2024 ETEC Zona Leste</p>
+
+            <ul class="nav col-md-4 justify-content-end">
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Inicio</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Cursos</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Instituição</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Departamentos</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Oportunidades</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Vestibulinho</a></li>
+            </ul>
+        </footer>
     </div>
 
 </body>
