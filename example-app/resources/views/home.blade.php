2d82dce0f7233c4440fc2e2414a73bda37826a6c
stxrkwas <maria.silva4476@etec.sp.gov.br>
2024-04-08 19:01:00 -0300

    Tá going...

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - Etec da Zona Leste</title>
+    <title>Home - ETEC Zona Leste</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/css/home.css">
@@ -13,37 +13,24 @@
 
 <body>
 
-    <!-- Mudar o tema do site -->
-    <div class="container text-center">
-        <div class="row">
-            <div class="col">
-                &nbsp;
-            </div>
-            <div class="col">
-                &nbsp;
-            </div>
-            <div class="col">
-                <!-- Icon para mudar o tema do site -->
-            </div>
-        </div>
-    </div>
-    <nav class="navbar navbar-expand-lg bg-body-white ">
+    <!-- Barra de navegação: -->
+    <nav class="navbar navbar-expand-lg bg-body-white">
         <div class="container-fluid ">
 
             <!-- Navbar com a logo da ETEC -->
-            <a class="navbar-brand" href="#">
-                <img src="/img/eteclogo.png" alt="Logomarca da ETEC da Zona Leste" width="100" height="94">
+            <a class="navbar-brand ms-5" href="#">
+                <img src="/img/eteclogo.png" alt="Logomarca da ETEC da Zona Leste" style="margin-left: 200px;" width="100px;" height="94px;">
             </a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse align-items-right" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav nav nav-underline ms-auto mb-2 mb-lg-0">
 
                     <!-- Início -->
                     <li class="nav-item">
-                        <a class="nav-link active" style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" aria-current="page" href="home">Início</a>
+                        <a class="nav-link " style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" aria-current="page" href="home">Início</a>
                     </li>
 
                     <!-- <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a> -->
@@ -99,31 +86,52 @@
 
                     <!-- Oportunidades -->
                     <li class="nav-item">
-                        <a class="nav-link active" style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" aria-current="page" href="index">Oportunidades</a>
+                        <a class="nav-link" style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" aria-current="page" href="index">Oportunidades</a>
                     </li>
 
                     <!-- Vestibulinho -->
                     <li class="nav-item">
-                        <a class="nav-link active" style="margin-top: 5px; margin-left: 10px; margin-right: 100px;" aria-current="page" href="index">Vestibulinho</a>
+                        <a class="nav-link" style="margin-top: 5px; margin-left: 10px; margin-right: 300px;" aria-current="page" href="index">Vestibulinho</a>
                     </li>
             </div>
         </div>
     </nav>
 
-    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center" style="width: 100%;">
+    <!-- Imagem: -->
+    <div class="position-relative overflow-hidden text-center"> <!-- p-3 p-md-5 m-md-3 -->
         <img src="/img/home.etec.webp" class="img-fluid" width="100%;" height="100%;"></img>
     </div>
 
-    <div class="row featurette">
-      <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span class="text-body-secondary">See for yourself.</span></h2>
-        <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
-      </div>
+    <!-- Barra de pesquisa:  -->
+    <div class="container text-center bg-body-tertiary">
+        <div class="row bg-body-tertiary"> <!-- align-items-center -->
+            <div class="col bg-body-tertiary">
+                &nbsp;
+            </div>
+            <div class="col bg-body-tertiary">
+                <div class="container-fluid mt-5 mb-5 bg-body-tertiary">
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" height="65px;">
+                    </form>
+                </div>
+            </div>
+            <div class="col bg-body-tertiary">
+                &nbsp;
+            </div>
+        </div>
     </div>
-    
+
+    <!-- Texto e imagem: -->
+    <div class="row featurette mt-5">
+        <div class="col-md-7 order-md-2 mt-5">
+            <h3 class="featurette-heading fw-normal lh-1">O que a ETEC pode fazer pelo seu FUTURO</h3>
+            <p class="lead lh-base text-align-justify">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+        </div>
+        <div class="col-md-5 order-md-1 mt-5">
+            <img src="/img/"></img>
+        </div>
+    </div>
+
 </body>
 
 </html>
