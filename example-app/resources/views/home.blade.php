ce36640eed118aafe0a4e9e74d0c27df5706adea
Malu⚡ <124840632+stxrkwas@users.noreply.github.com>
2024-04-08 09:29:27 -0300

    Update home.blade.php

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -105,6 +105,16 @@
             </div>
         </div>
     </nav>
+
+    <main>
+  <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
+    <div class="col-md-6 p-lg-5 mx-auto my-5">
+        <img src="/img/home.etec.webp" ></img>
+      </div>
+    </div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+  </div>
 </body>
 
 </html>
