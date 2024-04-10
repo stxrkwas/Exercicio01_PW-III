77eef437f91eae42e9f320e7afb24d7c1d65b75e
stxrkwas <maria.silva4476@etec.sp.gov.br>
2024-04-09 21:04:33 -0300

    It's a long way to the top (If you wanna rock n' roll)

--- example-app/resources/views/cursos.blade.php
+++ example-app/resources/views/cursos.blade.php
@@ -206,12 +206,12 @@
             <p class="col-md-4 mb-0 text-body-secondary">&copy; 2024 ETEC Zona Leste</p>
 
             <ul class="nav col-md-4 justify-content-end">
-                <li class="nav-item"><a href="home" class="nav-link px-2 text-body-secondary">Inicio</a></li>
-                <li class="nav-item"><a href="cursos" class="nav-link px-2 text-body-secondary">Cursos</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Instituição</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Departamentos</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Oportunidades</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Vestibulinho</a></li>
+                <li class="nav-item"><a href="/home" class="nav-link px-2 text-body-secondary">Inicio</a></li>
+                <li class="nav-item"><a href="/cursos" class="nav-link px-2 text-body-secondary">Cursos</a></li>
+                <li class="nav-item"><a href="/instituicao" class="nav-link px-2 text-body-secondary">Instituição</a></li>
+                <li class="nav-item"><a href="/departamentos" class="nav-link px-2 text-body-secondary">Departamentos</a></li>
+                <li class="nav-item"><a href="/oportunidades" class="nav-link px-2 text-body-secondary">Oportunidades</a></li>
+                <li class="nav-item"><a href="/vestibulinho" class="nav-link px-2 text-body-secondary">Vestibulinho</a></li>
             </ul>
         </footer>
     </div>
