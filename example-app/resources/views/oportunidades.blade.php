caed1b6a4312c4bbec2d7eea93cfeed49cbd2567
stxrkwas <maria.silva4476@etec.sp.gov.br>
2024-04-10 16:24:44 -0300

    Pausa pro lanchinho

---
+++ example-app/resources/views/oportunidades.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - ETEC Zona Leste</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
+</head>
+
+<body>
+
+    <!-- Barra de navegação: -->
+    <nav class="navbar navbar-expand-lg bg-body-white">
+        <div class="container-fluid ">
+
+            <!-- Navbar com a logo da ETEC -->
+            <a class="navbar-brand ms-5" href="#">
+                <img src="/img/eteclogo.png" alt="Logomarca da ETEC da Zona Leste" style="margin-left: 200px;" width="100px;" height="94px;">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse align-items-right" id="navbarSupportedContent">
+                <ul class="navbar-nav nav nav-underline ms-auto mb-2 mb-lg-0">
+
+                    <!-- Início -->
+                    <li class="nav-item">
+                        <a class="nav-link " style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" aria-current="page" href="home">Início</a>
+                    </li>
+
+                    <!-- <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a> -->
+
+                    <!-- Cursos -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="cursos" style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Cursos
+                        </a>
+
+                        <!-- Cursos da ETEC -->
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="cursos/#secao-adm">Administração</a></li>
+                            <li><a class="dropdown-item" href="cursos/#secao-contabilidade">Contabilidede</a></li>
+                            <li><a class="dropdown-item" href="cursos/#secao-ds">Desenvolvimento de Sistemas</a></li>
+                            <li><a class="dropdown-item" href="cursos/#secao-log">Logística</a></li>
+                            <li><a class="dropdown-item" href="cursos/#secao-rh">Recursos Humanos</a></li>
+                            <li><a class="dropdown-item" href="cursos/#secao-jur">Serviços Jurídicos</a></li>
+                        </ul>
+                    </li>
+
+                    <!-- Instituição -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" data-bs-toggle="dropdown" aria-expanded="false">
+                            Instituição
+                        </a>
+
+                        <!-- Opções -->
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="instituicao/#nossa-his"> História</a></li>
+                            <li><a class="dropdown-item" href="instituicao/#nossa-infra"> Infraestrutura</a></li>
+                            <li><a class="dropdown-item" href="instituicao/#plano-plurianual-de-gestao">Plano Plurianual de Gestão</a></li>
+                            <li><a class="dropdown-item" href="instituicao/#manual-do-aluno">Manual do Aluno</a></li>
+                            <li><a class="dropdown-item" href="instituicao/#regimento-comum-das-etecs">Regimento Comum das Etecs</a></li>
+                        </ul>
+                    </li>
+
+                    <!-- Departamentos -->
+                    <li class="nav-item dropdown ">
+                        <a class="nav-link dropdown-toggle" href="home" role="button" style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" data-bs-toggle="dropdown" aria-expanded="false">
+                            Departamentos
+                        </a>
+
+                        <!-- Cursos da ETEC -->
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="departamentos/#apm">APM - Associação de Pais e Mestres</a></li>
+                            <li><a class="dropdown-item" href="departamentos/#coor-de-cursos">Coordenação de Cursos</a></li>
+                            <li><a class="dropdown-item" href="departamentos/#direcao">Direção</a></li>
+                        </ul>
+                    </li>
+
+                    <!-- Oportunidades -->
+                    <li class="nav-item">
+                        <a class="nav-link" style="margin-top: 5px; margin-left: 10px; margin-right: 10px;" aria-current="page" href="index">Oportunidades</a>
+                    </li>
+
+                    <!-- Vestibulinho -->
+                    <li class="nav-item">
+                        <a class="nav-link" style="margin-top: 5px; margin-left: 10px; margin-right: 300px;" aria-current="page" href="index">Vestibulinho</a>
+                    </li>
+            </div>
+        </div>
+    </nav>
+
+</body>
+</html>
